943b4f15f96342d84f111f3ab9bccc74c6af1fb1
humblebeginning <manoj@electricsounds.co.uk>
2015-10-04 23:29:10 +0100

    Update groceryMain.php

--- groceryMain.php
+++ groceryMain.php
@@ -9,53 +9,52 @@
  *
  */
 
-	require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-	$url = "http://www.sainsburys.co.uk/webapp/wcs/stores/servlet/CategoryDisplay?listView=true&orderBy=FAVOURITES_FIRST&parent_category_rn=12518&top_category=12518&langId=44&beginIndex=0&pageSize=20&catalogId=10137&searchTerm=&categoryId=185749&listId=&storeId=10151&promotionId=#langId=44&storeId=10151&catalogId=10137&categoryId=185749&parent_category_rn=12518&top_category=12518&pageSize=20&orderBy=FAVOURITES_FIRST&searchTerm=&beginIndex=0&hideFilters=true";
+$url = "http://www.sainsburys.co.uk/webapp/wcs/stores/servlet/CategoryDisplay?listView=true&orderBy=FAVOURITES_FIRST&parent_category_rn=12518&top_category=12518&langId=44&beginIndex=0&pageSize=20&catalogId=10137&searchTerm=&categoryId=185749&listId=&storeId=10151&promotionId=#langId=44&storeId=10151&catalogId=10137&categoryId=185749&parent_category_rn=12518&top_category=12518&pageSize=20&orderBy=FAVOURITES_FIRST&searchTerm=&beginIndex=0&hideFilters=true";
 
+//
+// Process command line options
+//
+$options = getopt("u:h");
+if (isset($options['h']))
+{
+	echo "Usage: " . __FILE__ . " (-u url)" . PHP_EOL;
+	exit(0);
+}
 
-	//
-	// Process command line options
-	//
-	$options = getopt("u:h");
-	if (isset($options['h']))
-	{
-		echo "Usage: " . __FILE__ . " (-u url)" . PHP_EOL;
-		exit(0);
-	}
+if (isset($options['u']))
+{
+	$url = $options['u'];
+}
 
-	if (isset($options['u']))
-	{
-		$url = $options['u'];
-	}
-
-	//
-	// Create new instance of Grocery class
-	//
-	$groceryTest = new Grocery();
-
-
-	$logger = $groceryTest->GetLogger();
-	$logger->info("Starting");
-
-	//
-	// Get html of target url
-	//
-	$htmlContent = $groceryTest->GetWebPage($url);
+//
+// Create new instance of Grocery class
+//
+$groceryTest = new Grocery();
 
 
-	if (!$htmlContent)
-	{
-		$log->addError("Failed to get data");
-		exit(0);
-	}
-	else
-	{
-		// Run class method that processes HTML and creates output JSON file
-		// including any products found
-		$groceryTest->CreateJson($htmlContent);
-	}
+$logger = $groceryTest->GetLogger();
+$logger->info("Starting");
 
-	$logger->info("Finished");
+//
+// Get html of target url
+//
+$htmlContent = $groceryTest->GetWebPage($url);
+
+
+if (!$htmlContent)
+{
+	$log->addError("Failed to get data");
+	exit(0);
+}
+else
+{
+	// Run class method that processes HTML and creates output JSON file
+	// including any products found
+	$groceryTest->CreateJson($htmlContent);
+}
+
+$logger->info("Finished");
 
 ?>
